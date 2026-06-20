25ed906eaf0d456fd496f912680e81054438a72e
lisk77 <lisk77@protonmail.com>
2026-06-20 17:03:09 +0200

    feat: multi monitor popups

--- src/popup/mod.rs
+++ src/popup/mod.rs
@@ -47,11 +47,12 @@ pub struct Popup {
     layer_shell: LayerShell,
     shm: Shm,
     pool: SlotPool,
-    surfaces: BTreeMap<u32, NotificationSurface>,
+    surfaces: BTreeMap<u32, Vec<NotificationSurface>>,
 }
 
 struct NotificationSurface {
     notification: Arc<Notification>,
+    output: wl_output::WlOutput,
     layer: LayerSurface,
     configured: bool,
     buffer: Option<Buffer>,
@@ -104,35 +105,51 @@ impl Popup {
     fn show(&mut self, qh: &QueueHandle<Self>, notification: Arc<Notification>) {
         let id = notification.id;
 
-        if let Some(surface) = self.surfaces.get_mut(&id) {
-            surface.notification = notification;
-            self.draw(id, qh);
+        if let Some(surfaces) = self.surfaces.get_mut(&id) {
+            for surface in surfaces.iter_mut() {
+                surface.notification = notification.clone();
+            }
+            for index in 0..surfaces.len() {
+                self.draw(id, index, qh);
+            }
             return;
         }
 
+        let surfaces = self
+            .output_state
+            .outputs()
+            .map(|output| self.create_surface(qh, notification.clone(), output))
+            .collect();
+        self.surfaces.insert(id, surfaces);
+        self.restack();
+    }
+
+    fn create_surface(
+        &self,
+        qh: &QueueHandle<Self>,
+        notification: Arc<Notification>,
+        output: wl_output::WlOutput,
+    ) -> NotificationSurface {
         let wl_surface = self.compositor.create_surface(qh);
         let layer = self.layer_shell.create_layer_surface(
             qh,
             wl_surface,
             Layer::Overlay,
             Some("pigeond-notification"),
-            None,
+            Some(&output),
         );
         layer.set_anchor(Anchor::TOP | Anchor::RIGHT);
         layer.set_keyboard_interactivity(KeyboardInteractivity::None);
         layer.set_exclusive_zone(0);
         layer.set_size(CARD_WIDTH, CARD_HEIGHT);
 
-        self.surfaces.insert(
-            id,
-            NotificationSurface {
-                notification,
-                layer,
-                configured: false,
-                buffer: None,
-            },
-        );
-        self.restack();
+        NotificationSurface {
+            notification,
+            output,
+            layer,
+            configured: false,
+            buffer: None,
+        }
     }
 
     fn close(&mut self, id: u32) {
@@ -141,15 +158,21 @@ impl Popup {
     }
 
     fn restack(&self) {
-        for (index, surface) in self.surfaces.values().enumerate() {
+        for (index, surfaces) in self.surfaces.values().enumerate() {
             let top = TOP_MARGIN + index as i32 * (CARD_HEIGHT as i32 + CARD_GAP);
-            surface.layer.set_margin(top, RIGHT_MARGIN, 0, 0);
-            surface.layer.commit();
+            for surface in surfaces {
+                surface.layer.set_margin(top, RIGHT_MARGIN, 0, 0);
+                surface.layer.commit();
+            }
         }
     }
 
-    fn draw(&mut self, id: u32, _qh: &QueueHandle<Self>) {
-        let Some(surface) = self.surfaces.get(&id) else {
+    fn draw(&mut self, id: u32, output_index: usize, _qh: &QueueHandle<Self>) {
+        let Some(surface) = self
+            .surfaces
+            .get(&id)
+            .and_then(|surfaces| surfaces.get(output_index))
+        else {
             return;
         };
         if !surface.configured {
@@ -179,7 +202,11 @@ impl Popup {
             .expect("attach notification buffer");
         layer.commit();
 
-        if let Some(surface) = self.surfaces.get_mut(&id) {
+        if let Some(surface) = self
+            .surfaces
+            .get_mut(&id)
+            .and_then(|surfaces| surfaces.get_mut(output_index))
+        {
             surface.buffer = Some(buffer);
         }
     }
@@ -227,12 +254,18 @@ impl CompositorHandler for Popup {
 
 impl LayerShellHandler for Popup {
     fn closed(&mut self, _: &Connection, _: &QueueHandle<Self>, layer: &LayerSurface) {
-        if let Some(id) = self
-            .surfaces
-            .iter()
-            .find_map(|(id, surface)| (&surface.layer == layer).then_some(*id))
-        {
-            self.close(id);
+        if let Some((id, output_index)) = self.surfaces.iter().find_map(|(id, surfaces)| {
+            surfaces
+                .iter()
+                .position(|surface| &surface.layer == layer)
+                .map(|output_index| (*id, output_index))
+        }) {
+            self.surfaces.get_mut(&id).unwrap().remove(output_index);
+            if self.surfaces[&id].is_empty() {
+                self.close(id);
+            } else {
+                self.restack();
+            }
         }
     }
 
@@ -244,13 +277,14 @@ impl LayerShellHandler for Popup {
         _: LayerSurfaceConfigure,
         _: u32,
     ) {
-        if let Some(id) = self
-            .surfaces
-            .iter()
-            .find_map(|(id, surface)| (&surface.layer == layer).then_some(*id))
-        {
-            self.surfaces.get_mut(&id).unwrap().configured = true;
-            self.draw(id, qh);
+        if let Some((id, output_index)) = self.surfaces.iter().find_map(|(id, surfaces)| {
+            surfaces
+                .iter()
+                .position(|surface| &surface.layer == layer)
+                .map(|output_index| (*id, output_index))
+        }) {
+            self.surfaces.get_mut(&id).unwrap()[output_index].configured = true;
+            self.draw(id, output_index, qh);
         }
     }
 }
@@ -266,11 +300,42 @@ impl OutputHandler for Popup {
         &mut self.output_state
     }
 
-    fn new_output(&mut self, _: &Connection, _: &QueueHandle<Self>, _: wl_output::WlOutput) {}
+    fn new_output(&mut self, _: &Connection, qh: &QueueHandle<Self>, output: wl_output::WlOutput) {
+        let missing = self
+            .surfaces
+            .iter()
+            .filter_map(|(id, surfaces)| {
+                if surfaces.iter().any(|surface| surface.output == output) {
+                    None
+                } else {
+                    surfaces
+                        .first()
+                        .map(|surface| (*id, surface.notification.clone()))
+                }
+            })
+            .collect::<Vec<_>>();
+
+        for (id, notification) in missing {
+            let surface = self.create_surface(qh, notification, output.clone());
+            self.surfaces.get_mut(&id).unwrap().push(surface);
+        }
+        self.restack();
+    }
 
     fn update_output(&mut self, _: &Connection, _: &QueueHandle<Self>, _: wl_output::WlOutput) {}
 
-    fn output_destroyed(&mut self, _: &Connection, _: &QueueHandle<Self>, _: wl_output::WlOutput) {}
+    fn output_destroyed(
+        &mut self,
+        _: &Connection,
+        _: &QueueHandle<Self>,
+        output: wl_output::WlOutput,
+    ) {
+        self.surfaces.retain(|_, surfaces| {
+            surfaces.retain(|surface| surface.output != output);
+            !surfaces.is_empty()
+        });
+        self.restack();
+    }
 }
 
 impl ProvidesRegistryState for Popup {
